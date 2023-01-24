7ab236b4cca01ed8ba8285b6e52e7d2b6b99b551
Alexa spalato <alexaspalato@gmail.com>
2023-01-24 12:16:35 +0100

    serach form and headlessui modal

---
+++ app/components/search/SearchForm.js
@@ -0,0 +1,24 @@
+import { Form } from "@remix-run/react";
+import clsx from "clsx";
+
+const SearchForm = () => {
+  return (
+    <Form method="post" className="flex justify-between md:w-[90%]">
+      <input
+        type="text"
+        name="query"
+        placeholder="Search..."
+        className={clsx(
+          "w-full h-12",
+          "bg-teal-800",
+          "border-0 border-l-8 border-teal-200 focus:border-teal-600",
+          "text-teal-100",
+          "text-xl font-bold tracking-wide uppercase",
+          "focus:outline-none focus:ring-transparent focus:placeholder-transparent  placeholder-teal-200"
+        )}
+      />
+    </Form>
+  );
+};
+
+export default SearchForm;
