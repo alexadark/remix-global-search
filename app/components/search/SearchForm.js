2a78dacbb73f414b1d33abb44a26951d6fbc65ea
Alexa spalato <alexaspalato@gmail.com>
2023-01-27 13:41:48 +0100

    autofocus on input

--- app/components/search/SearchForm.js
+++ app/components/search/SearchForm.js
@@ -1,14 +1,24 @@
+import { useEffect, useRef } from "react";
 import { Form } from "@remix-run/react";
 import clsx from "clsx";
 
 const SearchForm = ({ setOpen }) => {
+  let formRef = useRef();
+  let inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
   return (
     <Form
+      ref={formRef}
       method="post"
       className="flex justify-between md:w-[90%]"
       onSubmit={() => setOpen(false)}
     >
       <input
+        ref={inputRef}
+        autofocus
         type="text"
         name="query"
         placeholder="Search..."
@@ -18,7 +28,7 @@ const SearchForm = ({ setOpen }) => {
           "border-0 border-l-8 border-teal-200 focus:border-teal-600",
           "text-teal-100",
           "text-xl font-bold tracking-wide uppercase",
-          "focus:outline-none focus:ring-transparent focus:placeholder-transparent  placeholder-teal-200"
+          "focus:outline-none focus:ring-transparent  placeholder-teal-200"
         )}
       />
     </Form>
