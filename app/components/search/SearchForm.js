1d8f7f38fa3e1fba7db1c424633555dae0d7f8a5
Alexa spalato <alexaspalato@gmail.com>
2023-02-03 16:22:30 +0100

    search form and results

--- app/components/search/SearchForm.js
+++ app/components/search/SearchForm.js
@@ -1,22 +1,29 @@
+import { useEffect, useRef } from "react";
 import clsx from "clsx";
-
+import { Form } from "@remix-run/react";
 const SearchForm = () => {
+  let inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
-    <form className="flex justify-between md:w-[90%]">
+    <Form method="post" className="flex justify-between md:w-[90%]">
       <input
         type="text"
         name="query"
         placeholder="Search..."
+        ref={inputRef}
         className={clsx(
           "w-full h-12",
           "bg-teal-800",
           "border-0 border-l-8 border-teal-200 focus:border-teal-600",
           "text-teal-100",
           "text-xl font-bold tracking-wide uppercase",
-          "focus:outline-none focus:ring-transparent focus:placeholder-transparent  placeholder-teal-200"
+          "focus:outline-none focus:ring-transparent  placeholder-teal-200"
         )}
       />
-    </form>
+    </Form>
   );
 };
 
